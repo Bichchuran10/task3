b1941e92ad39b1ce0b839b3de11f081d25db6493
Bichchuran10 <bichchuran10@gmail.com>
2022-11-07 20:28:58 +0530

    3rdn commit

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ console.log(headerTitle.textContent) //doesnt pay attention to the styling
 
 //headerTitle.textContent="Making Changes"
 
-let items=document.getElementById("items")
-items.lastElementChild.style.background="orange"
+//let items=document.getElementById("items")
+//items.lastElementChild.style.background="orange"
 
-headerTitle.style.borderBottom="5px solid black"
+//headerTitle.style.borderBottom="5px solid black"
 
-formHead=document.querySelector(".title")
+//formHead=document.querySelector(".title")
 
-formHead.style.color="green"
-formHead.style.fontFamily="Bold"
+//formHead.style.color="green"
+//formHead.style.fontFamily="Bold"
+
+
+let itemList=document.getElementsByClassName("list-group-item")
+
+for(let i=0 ; i<itemList.length;i++)
+{
+    itemList[2].style.backgroundColor="green"
+    itemList[i].style.fontWeight="Bold"
+}
+
+
+
