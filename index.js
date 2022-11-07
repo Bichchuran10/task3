363cb512cc5f9553de1157abe0c952b001441eb3
Bichchuran10 <bichchuran10@gmail.com>
2022-11-07 21:08:18 +0530

    4th commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let headerTitle=document.getElementById("header-title")
-console.log(headerTitle.innerText) //pays attention to the styling
-console.log(headerTitle.textContent) //doesnt pay attention to the styling
+//let headerTitle=document.getElementById("header-title")
+//console.log(headerTitle.innerText) //pays attention to the styling
+//console.log(headerTitle.textContent) //doesnt pay attention to the styling
 
 //headerTitle.textContent="Making Changes"
 
@@ -15,13 +15,28 @@ console.log(headerTitle.textContent) //doesnt pay attention to the styling
 //formHead.style.fontFamily="Bold"
 
 
-let itemList=document.getElementsByClassName("list-group-item")
+//let itemList=document.getElementsByClassName("list-group-item")
 
-for(let i=0 ; i<itemList.length;i++)
-{
-    itemList[2].style.backgroundColor="green"
-    itemList[i].style.fontWeight="Bold"
-}
+//for(let i=0 ; i<itemList.length;i++)
+//{
+    //itemList[2].style.backgroundColor="green"
+    //itemList[i].style.fontWeight="Bold"
+//}
+
+
+//new item by className
+
+let newItem=document.getElementsByClassName("new-item")
+newItem[0].style.color="blue"
+
+
+//new item by tagsName
+let liTag=document.getElementsByTagName("li")
+liTag[4].style.backgroundColor="red"
+
+
+
+
 
 
 
