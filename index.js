c8a26107177324003b23f188ddc2e03dfd5cea4a
Bichchuran10 <bichchuran10@gmail.com>
2022-11-07 21:38:19 +0530

    5th commit

--- index.js
+++ index.js
@@ -26,15 +26,33 @@
 
 //new item by className
 
-let newItem=document.getElementsByClassName("new-item")
-newItem[0].style.color="blue"
+//let newItem=document.getElementsByClassName("new-item")
+//newItem[0].style.color="blue"
 
 
 //new item by tagsName
-let liTag=document.getElementsByTagName("li")
-liTag[4].style.backgroundColor="red"
+//let liTag=document.getElementsByTagName("li")
+//liTag[4].style.backgroundColor="red"
 
 
+//query Selector
+//taking the second item by nth-child(n) property
+let secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor="#f4f4f4" //didn't make it green instead making it this color since in the next task we've to make the textcolor green which will make the text invisible
+
+
+//hiding the third item using visibility property
+let thirdItem=document.querySelector(".list-group-item:nth-child(3)");
+thirdItem.style.visibility="hidden"
+
+
+//query Selector All
+let allLi=document.querySelectorAll(".list-group-item")
+allLi[1].style.color="green"; 
+
+let odd=document.querySelectorAll("li:nth-child(odd)");
+for(let i=0;i<odd.length;i++)
+odd[i].style.backgroundColor="green"
 
 
 
