2e13859ee9c6912a8bb06e28fa3b83af7dba1c26
Bichchuran10 <bichchuran10@gmail.com>
2022-11-08 14:01:00 +0530

    added a new list item

--- index.js
+++ index.js
@@ -1,90 +1,28 @@
-//Traversing the DOM
+let form=document.getElementById('addForm')
+let itemList=document.getElementById('items')
 
-let itemList=document.querySelector('#items');
+//adding an event listener to the form submission
+form.addEventListener('submit',addItem)
 
-//parentNode
-//console.log(itemList.parentNode) //div with id=main
-//itemList.parentNode.style.backgroundColor="black"
-//console.log(itemList.parentNode.parentNode) //div with class container
+function addItem(e)
+{
+    e.preventDefault()
 
+    //we need to take the input value and store it in a variable
+    let newItem=document.getElementById('item').value
 
-//1.parentElement
-console.log(itemList.parentElement) //div with id=main
-itemList.parentElement.style.backgroundColor="grey"
-console.log(itemList.parentElement.parentElement) //div with class container
+    //we need to create a new li for it
+    let li=document.createElement('li')
 
-//childNode
-//console.log(itemList.childNode) // includes everything from text node,line breaks etc
+    //adding the li class
+    li.className="list-group-item"
 
-//children
-console.log(itemList.children) //only the elements i.e no text nodes , line breaks
+    //to get the text from the input
+    //adding a text node to the li with the input value
+    li.appendChild(document.createTextNode(newItem))
 
-//2. lastElementChild
-itemList.lastElementChild.style.color="red" //colors item 4
-
-//3. lastChild
-console.log(itemList.lastChild)
-
-//6.firstChild
-console.log(itemList.firstChild) //text node
-
-//5.firstElementChild
-//console.log(itemList.firstElementChild)
-itemList.firstElementChild.style.color="orange"
-
-//7.nextSibling
-let firsth2=document.querySelector('.title')
-console.log(firsth2.nextSibling) //text node
-
-//8.nextElementSibling
-console.log(firsth2.nextElementSibling) //form will be its next sibling
-
-//9. previousSibling
-console.log(itemList.previousSibling) //text node
-
-//10. previousElementSibling
-console.log(itemList.previousElementSibling) //h2 will be its previous sibling
-itemList.previousElementSibling.style.backgroundColor="#f4f4f4"
+    //adding li to the list of items
+    itemList.appendChild(li)
 
 
-//create a div 
-//11.createElement
-let newDiv=document.createElement("div")
-
-//add class
-newDiv.className="hello"
-//add id
-newDiv.id="hello"
-
-//add attribute
-//12.setAttribute
-newDiv.setAttribute("title","hello Div")
-
-//create a text node
-//13. createTextNode
-let newDivText=document.createTextNode("HEllo")
-
-//Add text to div
-//14. appendChild()
-newDiv.appendChild(newDivText)
-
-
-let container= document.querySelector('header .container')
-let h1=document.querySelector('header h1')
-
-console.log(newDiv)
-
-container.insertBefore(newDiv,h1)
-//adding HEllo before item 1
-
-
-let allItems=document.querySelector('div .list-group') //div with class card card-body is the parent of ul
-console.log(allItems.parentElement)
-let allList=document.querySelector('div li') //in div we are taking li
-console.log(allList)
-
-allItems.insertBefore(newDiv,allList) //before first li
-
-
-
-
+}
